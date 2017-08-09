c0b5aa078dc674618246133bb3296fa15c4ae514
maniaxiom <manik15053@iiitd.ac.in>
2017-08-09 16:46:10 +0530

    Commented - MutInt.java

--- MutInt.java
+++ MutInt.java
@@ -1,20 +1,27 @@
-import java.util.PriorityQueue;
+// CLASS DESCRIPTION:
+// Integer Class to store and efficiently increment the integral frequencyss when looked up in a HashMap.
+// Priority Queue topk is used for some MutInt objects where it stores the top k most occuring words after a given phrase.
 
+import java.util.PriorityQueue;
 
 public class MutInt implements Comparable<MutInt>{
 	int val = 1;
 	boolean isCommon = false;
 	int position=0;
 	PriorityQueue<Pair<String,Integer>> topk;
+
 	public MutInt(){
 		topk = new PriorityQueue<Pair<String,Integer>>();
 	}
+
 	public void increment(){
 		this.val++;
 	}
+
 	public int get(){
 		return this.val;
 	}
+	
 	@Override
 	public int compareTo(MutInt o) {
 		if(this.val>o.val){
