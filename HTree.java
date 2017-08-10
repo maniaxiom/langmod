0dcb86ac5c436c77fa5718e25c22fe95ca4ac21f
maniaxiom <manik15053@iiitd.ac.in>
2017-08-10 20:47:04 +0530

    Commented - HTree.java; Included readme: procedure to understand code

--- HTree.java
+++ HTree.java
@@ -1,3 +1,25 @@
+// CLASS DESCRIPTION:
+// An object of this class holds a recursive tree data structure of Hashmaps.
+// This is used for find the next set of POS tags after a particular word in a sentence efficiently.
+// Eg.) We have the following POS Tags - A,B,C,D,E.
+// We have POS tags of sentences in corpus as in: A->B->C , A->B->D, B->A->E, B->B->D
+// We have a root HTree Object in Tagger class called POSTree (Let's call the object root. 
+// KEY: P.options = [{Q, HTree(Q)=PQ}] 
+// This means that the Hashmap options of object referred to by P contains Key String Q and it's value is a new HTree, which we will refer by PQ.
+// This is for representation purpose only. In the actual working, we return the actual HTree object to look for further POS options, instead of referring them by name.
+// 
+// root.options = [{A, HTree(A)=A},{B, HTree(B)=B}]
+// A.options = [{B, HTree(B)=AB}]
+// AB.options = [{C, HTree(C)=ABC}, {D, HTree(D)=ABD}]
+// B.options = [{A, HTree(A)=BA}, {B, HTree(B)=BB}]
+// BA.options = [{E, HTree(E)=BAE]
+// BB.options = [{D, HTree(D)=BBD}]
+// 
+// Hence if I have a sentence that starts with POS Tag A. The next possible tags are the keys in A.options, i.e, B.
+// Then the next possible tags are the keys in AB.options, i.e., C and D.
+// 
+
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -5,15 +27,18 @@ import java.util.Map;
 
 
 public class HTree {
+	// options contains list of next possible tags for the current HTree instance.
 	HashMap<String, HTree> options;
 	HTree(){
 		options = new HashMap<String, HTree>();
 	}
 	
+	// Return next set of possible tags
 	public HashSet<String> returnKeys(){
 		return (HashSet<String>) options.keySet();
 	}
 	
+	// Check if a tag is possible after the give HTree object
 	public boolean searchElement(String tagname){
 		if(options.containsKey(tagname)){
 			return true;
@@ -21,6 +46,7 @@ public class HTree {
 		return false;
 	}
 	
+	// Add in a new next possible POS tag if it does not exist
 	public HashMap<String,HTree> searchAndAddElement(String tagname, HashMap<String, HTree> opt){
 		HTree curr = opt.get(tagname);
 		if(curr!=null){
@@ -32,6 +58,7 @@ public class HTree {
 		}
 	}
 	
+	// Takes an arraylist in order of POS tags of a sentence and adds it to the POSTree Data Structure
 	public boolean addToTree(ArrayList<String> pos_sent){
 		HashMap<String,HTree> currBranch = options;
 		for(String tagname: pos_sent){
@@ -40,6 +67,7 @@ public class HTree {
 		return false;
 	}
 	
+	// Function to Print the entire POS Tree Recursively
 	public void printTree(HashMap<String, HTree> opt, int prevspace){
 		if(opt.size()==0){
 			System.out.println();
@@ -60,8 +88,8 @@ public class HTree {
 				tbprinted += entry.getKey()+"->";
 				System.out.print(tbprinted);
 				printTree(entry.getValue().options, tbprinted.length());
-			}
-			i++;
+			
+}			i++;
 		}
 	}
 	
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # langmod
 Language Model based True-False statement generator
+
+To understand the code via the comments, read the files in the following order:
+1) Pair.java
+2) MutInt.java
+3) HTree.java
+4) Tagger.java
